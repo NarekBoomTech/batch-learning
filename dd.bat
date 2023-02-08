142e047981876bf88826880583dbb6aececfb5ee
NarekBoomTech <nareks@boomte.ch>
2023-02-08 11:23:56 +0400

    Update boom-calendar

--- dd.bat
+++ dd.bat
@@ -3,7 +3,7 @@ set commitName="Update boom-calendar"
 set prTitle="Update boom-calendar pull request"
 
 call npm i --prefix ../batch-learning is-prime
-PAUSE
+
 git config --add --bool push.autoSetupRemote true
 git add .
 git commit -m %commitName%
